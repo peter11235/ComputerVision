14ba16f8987b348d85efe0fdc33469a76247bb4b
Peter Traunfeld <ptraunfeld@elon.edu>
2016-04-27 13:51:24 -0400

    Created skeleton application

---
+++ ExampleApplication/src/edu/elon/example/myapp/Classifier.java
@@ -0,0 +1,16 @@
+package edu.elon.example.myapp;
+import org.opencv.core.*;
+import java.lang.Runtime.*;
+
+public class Classifier {
+
+	public Classifier()
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
